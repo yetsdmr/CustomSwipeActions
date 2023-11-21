b21fb2da75ebcd0662c13ff678b9a71d5f656b3f
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-11-21 14:25:57 +0300

    Initial Commit

---
+++ CustomSwipeActions/CustomSwipeActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwipeActionsApp.swift
+//  CustomSwipeActions
+//
+//  Created by Yunus Emre Taşdemir on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwipeActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
